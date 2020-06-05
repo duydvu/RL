d402f7b7d3abbee1ce4b0cbb374b05b2cce96cdf
Vũ Đức Duy <duyisking1060@gmail.com>
2020-06-05 21:33:59 +0700

    Update SMA

--- lib/utils.py
+++ lib/utils.py
@@ -1,8 +1,28 @@
 import numpy as np
 
 def SMA(data: np.array, degree: int):
-    data = np.append([data[0]] * (degree - 1), data)
-    smoothed = np.array([
-        np.roll(data, i) for i in range(degree)
-    ]).T
-    return np.sum(smoothed, axis=1)[degree - 1:] / degree
+    data = np.concatenate(([data[0]] * (degree - 1), data, [0]))
+    slice_sum = sum(data[:degree])
+    result = []
+    for i in range(1, len(data) - degree + 1):
+        result.append(slice_sum)
+        slice_sum += data[i + degree - 1] - data[i - 1]
+    return np.array(result) / degree
+
+
+if __name__ == "__main__":
+    import unittest
+    from numpy import testing
+
+    class TestSMA(unittest.TestCase):
+        def test_result(self):
+            arr = np.array([1, 2, 3, 4, 5])
+            res = SMA(arr, 2)
+            testing.assert_array_equal(res, [1, 1.5, 2.5, 3.5, 4.5])
+
+        def test_result_2(self):
+            arr = np.array([1, 2, 3, 4, 5])
+            res = SMA(arr, 3)
+            testing.assert_almost_equal(res, [1, 1.33333333, 2, 3, 4])
+
+    unittest.main()
